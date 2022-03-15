603a1959ac04c75095c36dec31c1c3fd4e14784e
ulldecorb <ausias.bertran@gmail.com>
2022-03-16 00:20:24 +0100

    Add axios library Connect app to api Prop response to component

--- pokedesk/src/App.jsx
+++ pokedesk/src/App.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import PokemonList from './components/PockemonList';
 // import './App.css';
 
 function App() {
-  // eslint-disable-next-line no-unused-vars
-  const [pokemon, setPokemon] = useState(['pikachu', 'liquideitor']);
+  const [pokemon, setPokemon] = useState([]);
+  const [currentUrl, setCurrentUrl] = useState(['https://pokeapi.co/api/v2/pokemon']);
+  const [nextUrl, setNextUrl] = useState();
+  const [previousUrl, setPreviousUrl] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(currentUrl).then((res) => {
+      setLoading(false);
+      setNextUrl(res.data.next);
+      setPreviousUrl(res.data.previous);
+      setPokemon(res.data.results.map((p) => p.name));
+    });
+  }, [currentUrl]);
+
+  if (loading) return 'Loading...';
 
   return (
     <PokemonList pokemon={pokemon} />
